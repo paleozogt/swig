7d1bc1d1ae0b98eb07256f86997df6ae002016e2
Swati Sharma <itawswati@gmail.com>
2012-07-23 17:29:17 +0000

    Refactored Objective C Module code and Typemaps Ashish Patch:(ashishs99@gmail.com)
    git-svn-id: https://swig.svn.sourceforge.net/svnroot/swig/branches/gsoc2012-objc@13389 626c5289-ae23-0410-ae9c-e8d60b6d4f22

--- Source/Modules/swigmain.cxx
+++ Source/Modules/swigmain.cxx
@@ -37,7 +37,7 @@ extern "C" {
   Language *swig_php(void);
   Language *swig_php4(void);
   Language *swig_php5(void);
-  Language *swig_objc(void);
+  Language *swig_objectivec(void);
   Language *swig_ocaml(void);
   Language *swig_octave(void);
   Language *swig_pike(void);
@@ -81,7 +81,7 @@ static swig_module modules[] = {
   {"-lua", swig_lua, "Lua"},
   {"-modula3", swig_modula3, "Modula 3"},
   {"-mzscheme", swig_mzscheme, "Mzscheme"},
-  {"-objc", swig_objc, "Objective C"},
+  {"-objc", swig_objectivec, "OBJECTIVEC"},
   {"-ocaml", swig_ocaml, "Ocaml"},
   {"-octave", swig_octave, "Octave"},
   {"-perl", swig_perl5, "Perl"},
